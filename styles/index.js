55bf53e6caec7505d7f2913a1b736f6924527350
pwoofter <phoebewoofter@gmail.com>
2024-08-02 15:43:35 -0700

    boilerplate for other pages and simple js added to scrollbar

--- styles/index.js
+++ styles/index.js
@@ -1,1 +1,14 @@
-let scroll = document.getElementById = 'banner';
+let scrollBar = document.getElementById('banner');
+
+function scrollChange() {
+    scrollBar.style.backgroundColor = '#f6d9f7';
+    scrollBar.style.color = '#ff0000';
+}
+
+function scrollBack() {
+    scrollBar.style.backgroundColor = '#ff0000';
+    scrollBar.style.color = '#f6d9f7';
+}
+
+scrollBar.onmouseover = scrollChange;
+scrollBar.onmouseout = scrollBack;
